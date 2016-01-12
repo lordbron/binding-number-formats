b1888c743ab79fe48cc80f196b47dc47444396c6
lordbron <noreply>
2016-01-12 15:56:08 +0000

    Update component ui/main.reel/main.js

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -8,9 +8,28 @@ var Component = require("montage/ui/component").Component;
  * @extends Component
  */
 exports.Main = Component.specialize(/** @lends Main# */ {
-    constructor: {
-        value: function Main() {
-            this.super();
+    enterDocument: {
+        value: function (firstTime) {
+            if (firstTime) {
+            }
+        }
+    },
+    retrievedData: { value: null},
+    getJSONData: {
+        value: function () {
+            var me = this;
+            var xhr = new XMLHttpRequest();
+            var webServicePath = "core/model/data.json";
+            xhr.open('GET',webServicePath);
+            xhr.responseType = 'text';
+            xhr.onload = function(event) {
+                if (this.status == 200) {
+                    me.appModel["fieldsData"] = JSON.parse(event.target.response);
+                } else {
+                    alert("No web service call configured yet, but we're sending data to a fake one anyways.");
+                }
+            };
+            xhr.send();
         }
     }
 });
